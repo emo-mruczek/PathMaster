e6a4c076b50b541c457ec26397246af6cc4e609d
emo-mruczek <krokcia1@gmail.com>
2024-10-12 18:42:28 +0200

    dupa

--- app/src/main/java/pl/asiaki/pathmaster/Wallet.kt
+++ app/src/main/java/pl/asiaki/pathmaster/Wallet.kt
@@ -3,13 +3,27 @@ package pl.asiaki.pathmaster
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun WalletComposable(amount: UInt) {
     Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceEvenly) {
-
+                Text(
+                    text = "WALLET"
+                )
+                //TODO: buttor
+                Text(
+                    text = "Check balance ->"
+                )
     }
+}
+
+@Preview
+@Composable
+fun WalletComposablePreview(){
+    WalletComposable(2137u)
 }

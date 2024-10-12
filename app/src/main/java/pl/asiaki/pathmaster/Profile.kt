e4d958f5c8faa11f5160de39a38563faae7ebad4
emo-mruczek <krokcia1@gmail.com>
2024-10-12 15:42:24 +0200

    add: macius

--- app/src/main/java/pl/asiaki/pathmaster/Profile.kt
+++ app/src/main/java/pl/asiaki/pathmaster/Profile.kt
@@ -1,16 +1,39 @@
 package pl.asiaki.pathmaster
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 
 @Composable
-fun Profile() {
-    val image = painterResource(R.drawable.blank_avatar)
-    Image(
-        painter = image,
-        contentDescription = "avatar"
-    )
+fun Profile(name: String = "Maciuś", lvl: Int = 21) {
+    Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Column( horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(
+                    text = name,
+                    color = Black,
+                    fontSize = 7.5.em
+                )
+                Text(
+                    text = "Level: $lvl",
+                    color = Black,
+                    fontSize = 6.em
+                )
+            }
+        val image = painterResource(R.drawable.blank_avatar)
+        Image(
+            painter = image,
+            contentDescription = "avatar"
+        )
+    }
 }
 

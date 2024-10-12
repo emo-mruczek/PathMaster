4b0b9ee67d38ad050e7fd43c41d995304f80cebc
emo-mruczek <krokcia1@gmail.com>
2024-10-12 22:01:54 +0200

    update: fonts

---
+++ app/src/main/java/pl/asiaki/pathmaster/FontFamily.kt
@@ -0,0 +1,19 @@
+package pl.asiaki.pathmaster
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+
+val robotoFamily = FontFamily(
+    Font(R.font.robotolight, FontWeight.Light),
+    Font(R.font.robotolightitalic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.robotoregular, FontWeight.Normal),
+    Font(R.font.robotoitalic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.robotomedium, FontWeight.Medium),
+    Font(R.font.robotobold, FontWeight.Bold),
+    Font(R.font.robotomediumitalic, FontWeight.Medium, FontStyle.Italic),
+    Font(R.font.robotobolditalic, FontWeight.Bold, FontStyle.Italic),
+
+)
+
--- app/src/main/java/pl/asiaki/pathmaster/Wallet.kt
+++ app/src/main/java/pl/asiaki/pathmaster/Wallet.kt
@@ -1,7 +1,9 @@
 package pl.asiaki.pathmaster
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,39 +14,57 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import pl.asiaki.pathmaster.ui.theme.Orange
+import pl.asiaki.pathmaster.ui.theme.Pink
+import pl.asiaki.pathmaster.ui.theme.White
 
 @Composable
 fun WalletComposable(amount: UInt) {
-    Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceEvenly) {
-                Text(
-                    text = "WALLET"
-                )
-                //TODO: buttor
-                Text(
-                    text = "Check balance ->"
-                )
-    }
-    Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceEvenly) {
-
-        val image = painterResource(R.drawable.hat)
-        Image(
-            painter = image,
-            contentDescription = "MsT icon",
-            modifier = Modifier
-                .size(100.dp)
-                .padding(20.dp)
-        )
-        //TODO: buttor
-        Text(
-            text = "$amount"
-        )
-        Text(
-            text = "MsT"
-        )
+    val brush = Brush.linearGradient(listOf(Orange, Pink))
+    Box(Modifier.background(brush)) {
+        Row(
+            Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(
+                text = "WALLET",
+                color = White
+            )
+            //TODO: buttor
+            Text(
+                text = "Check balance ->",
+                color = White
+            )
+        }
+        Row(
+            Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            val image = painterResource(R.drawable.hat)
+            Image(
+                painter = image,
+                contentDescription = "MsT icon",
+                modifier = Modifier
+                    .size(100.dp)
+                    .padding(20.dp)
+            )
+            //TODO: buttor
+            Text(
+                text = "$amount",
+                color = White
+            )
+            Text(
+                text = "MsT",
+                color = White
+            )
+        }
     }
 }
 
